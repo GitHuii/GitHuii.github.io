482363830fcacfd3a5392bcf011ca8bc63e96fad
Huii <nvhuyzxc@gmail.com>
2024-11-06 01:29:49 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -13,19 +13,3 @@ overlay.addEventListener('click',()=>{
     menuBtn.classList.toggle("open");
     overlay.classList.toggle("show");
 });
-
-const starContainer = document.querySelector('.starry-background');
-// Hàm tạo ngôi sao ngẫu nhiên
-function createStar() {
-    const star = document.createElement('div');
-    star.classList.add('star');
-    star.style.left = `${Math.random() * 100}vw`;
-    star.style.top = `${Math.random() * 100}vh`;
-    star.style.width = `${Math.random() * 3 + 1}px`;
-    star.style.height = star.style.width;
-    starContainer.appendChild(star);
-}
-// Tạo nhiều ngôi sao
-for (let i = 0; i < 100; i++) {
-    createStar();
-}
